340e0adaf001cccf898d19d58a94e7aca1eda2eb
N1192518 <mathew.madzibanyika2024@my.ntu.ac.uk>
2024-11-10 16:18:47 +0000

    # Voting analysis project.py
    Problem encountered. I didn't increment the option for numbers. Will try using two tabs for alignment.

---
+++ # Voting analysis project.py
@@ -0,0 +1,11 @@
+# Voting analysis project
+
+# List of options and welcoming user
+options = ["List the constituencies", "List the MPS","List the parties"]
+
+print("Welcome to the election analysis software")
+print("Choose your option - use the number")
+print("Option Number\tOption")
+optionNumber = 0
+for option in options:
+    print(optionNumber, "\t", option)
