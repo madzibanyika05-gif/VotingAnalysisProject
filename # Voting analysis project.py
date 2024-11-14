de55b1a9d24e2cd9c5f253f4be97a56feda3c91b
N1192518 <mathew.madzibanyika2024@my.ntu.ac.uk>
2024-11-14 13:49:05 +0000

    Update 3
    Corrected numbers so it starts from 1, however, struggling to connect the file

--- # Voting analysis project.py
+++ # Voting analysis project.py
@@ -1,12 +1,20 @@
 # Voting analysis project
 
 # List of options and welcoming user
-options = ["List the constituencies", "List the MPS","List the parties"]
+options = ["Analyse the percentage of voters who voted and didn't vote", "List by constituency", "Analyse percentage by gender","List the MPS","List the parties"]
 
 print("Welcome to the election analysis software")
 print("Choose your option - use the number")
 print("Option Number\tOption")
-optionNumber = 0
+optionNumber = 1
 for option in options:
     print(optionNumber, "\t", option)
     optionNumber += 1
+
+import csv
+
+def load_data(filename):
+    aylist = []
+    with open(filename) as edited:
+        edited_data = csv.reader(edited, delimiter=',')
+        next(numbers_data) #skip the header
