82bb68f64b01aa22693ed059b507da178097c36b
N1192518 <mathew.madzibanyika2024@my.ntu.ac.uk>
2024-11-15 14:04:48 +0000

    reading file but error
    Code is now reading file, had problems at first but managed to make the code access the csv via different folders.

--- # Voting analysis project.py
+++ # Voting analysis project.py
@@ -13,8 +13,47 @@ for option in options:
 
 import csv
 
-def load_data(filename):
-    aylist = []
-    with open(filename) as edited:
-        edited_data = csv.reader(edited, delimiter=',')
-        next(numbers_data) #skip the header
+class Constituency:
+    def __init__(self,t):
+        self.details = t
+    def __str__(self):
+        return self.details['CName']+" "+self.details['Party']
+    def GetCountry(self):
+        return self.details['Country']
+    def GetType(self):
+        return self.details['CType']
+        
+
+constituencies = []
+#partyToCount = input("Party code to count, Lab, Con, LD, RUK, IND")
+parties = ["Lab","Con","LD","RUK","Green","IND","SNP","PC","DUP","SF","SDLP","UUP","APNI"]
+mpCount = []
+for p in parties:
+    mpCount.append(0)
+#mpCount = [0,0,0,0,0,0]
+#getting constituency data from csv file aka reading the csv file
+with open('Documents/GitHub/VotingAnalysisProject/EditedData.csv', newline='') as csvfile:
+
+
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        #print(row)
+        counter1 = 1
+        for party in parties:
+            if row['First party'].lower() == party.lower():
+                mpCount[counter1] += 1
+            counter1 += 1    
+        constituency = {'CName':row['Constituency name'],'RName':row['Region name'],'Country':row['Country name'],'CType':row['Constituency type'],'Party':row['First party']}
+        con = Constituency(constituency)
+        constituencies.append(con)
+    counter1 = 0
+    for party in parties:
+        print(party, " got ",mpCount[counter1], " mps")
+        counter1 += 1
+for con in constituencies:
+    print(con)
+plt.bar(parties,mpCount)
+plt.show()
+next =input()
+plt.pie(mpCount)
+plt.show()
